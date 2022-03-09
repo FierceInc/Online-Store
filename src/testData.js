cc96e1bc414b61c0e40194a4083b30602c46a30c
FierceInc <fierceincii@gmail.com>
2022-03-09 09:37:31 +0200

    Added testData and Images

---
+++ src/testData.js
@@ -0,0 +1,223 @@
+let sneakers = [
+  {
+    image: "https://i.ibb.co/BwqbWNC/Nike-Air-Monarch-IV-Blue.png",
+    name: 'Nike-Air-Monarch-IV-Blue',
+    id: "Nike-Air-Monarch-IV-Blue",
+    price: 1899.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/SJSxLF6/Nike-Air-Force-1-Sage-Low-Blue.png",
+    name: "Nike-Air-Force-1-Sage-Low-Blue",
+    id: "Nike-Air-Force-1-Sage-Low-Blue",
+    price: 1699.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image:"https://i.ibb.co/NK2m3yv/Nike-Air-Force-1-Sage-Low-True-Berry-Blue.png" ,
+    name: 'Nike-Air-Force-1-Sage-Low-True-Berry-Blue',
+    id: "Nike-Air-Force-1-Sage-Low-True-Berry-Blue",
+    price: 1050.95,
+    size: "7",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/Gdg1xQJ/Nike-Air-Max-270-React-Blue.png",
+    name: 'Nike-Air-Max-270-React-Blue',
+    id: "Nike-Air-Max-270-React-Blue",
+    price: 2099.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/3mJWF4s/Nike-WMNS-Air-Force-Blue.png" ,
+    name: 'Nike-WMNS-Air-Force-Blue',
+    id: "Nike-WMNS-Air-Force-Blue",
+    price: 1645.96,
+    size: "6",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/0BQmLhx/Adidas-Stan-Smith-Blue.png",
+    name: 'Adidas-Stan-Smith-Blue',
+    id: "Adidas-Stan-Smith-Blue",
+    price: 1399.95,
+    size: "4",
+    brand: "Adidas",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/zJ2Byn9/Nike-Air-Monarch-IV-Green.png",
+    name: 'Nike-Air-Monarch-IV-Green',
+    id: "Nike-Air-Monarch-IV-Green",
+    price: 1899.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/4pbDYWn/Nike-Air-Force-1-Sage-Low-Green.png",
+    name: 'Nike-Air-Force-1-Sage-Low-Green',
+    id: "Nike-Air-Force-1-Sage-Low-Green",
+    price: 1654.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/2Wc6mHY/Nike-Air-Force-1-Sage-Low-True-Berry-Green.png",
+    name: 'Nike-Air-Force-1-Sage-Low-True-Berry-Green',
+    id: "Nike-Air-Force-1-Sage-Low-True-Berry-Green",
+    price: 2093.80,
+    size: "7",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/qdK0s2L/Nike-Air-Max-270-React-Green.png",
+    name: 'Nike-Air-Max-270-React-Green',
+    id: "Nike-Air-Max-270-React-Green",
+    price: 2200.95,
+    size: "7",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/LZj2pdk/Nike-WMNS-Air-Force-Pink.png",
+    name: 'Nike-WMNS-Air-Force-Pink',
+    id: "Nike-WMNS-Air-Force-Pink",
+    price: 1499.95,
+    size: "6",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/C55ZGtP/Adidas-Stan-Smith-Green.png",
+    name: 'Adidas-Stan-Smith-Green',
+    id: "Adidas-Stan-Smith-Green",
+    price: 1560.75,
+    size: "6",
+    brand: "Adidas",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/RS47yrr/Nike-Air-Max-270-React-Red.png",
+    name: 'Nike-Air-Max-270-React-Red',
+    id: "Nike-Air-Max-270-React-Red",
+    price: 1299.95,
+    size: "6",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+
+  {
+    image: "https://i.ibb.co/8xZY1Ts/Nike-Air-Force-1-Sage-Low-True-Berry-Red.png" ,
+    name: 'Nike-Air-Force-1-Sage-Low-True-Berry-Red',
+    id: "Nike-Air-Force-1-Sage-Low-True-Berry-Red",
+    price: 1899.95,
+    size: "5",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/h7HWVxz/Nike-Air-Monarch-IV-Red.png" ,
+    name: 'Nike-Air-Monarch-IV-Red',
+    id: "Nike-Air-Monarch-IV-Red",
+    price: 1800.95,
+    size: "4",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/SBkDMYr/Nike-Air-Max-270-React-Pink.png",
+    name: 'Nike-Air-Max-270-React-Pink',
+    id: "Nike-Air-Max-270-React-Pink",
+    price: 1399.95,
+    size: "7",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/QX2s5Hm/Nike-Air-Force-1-Sage-Low-True-Berry-Pink.png" ,
+    name: 'Nike-Air-Force-1-Sage-Low-True-Berry-Pink',
+    id: "Nike-Air-Force-1-Sage-Low-True-Berry-Pink",
+    price: 2033.95,
+    size: "7",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/FJWthnD/Nike-Air-Force-1-Sage-Low-Pink.png",
+    name: 'Nike-Air-Force-1-Sage-Low-Pink',
+    id: "Nike-Air-Force-1-Sage-Low-Pink",
+    price: 1870.85,
+    size: "6",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+  {
+    image: "https://i.ibb.co/vPfVd4f/Nike-Air-Monarch-IV-Pink.png",
+    name: 'Nike-Air-Monarch-IV-Pink',
+    id: "Nike-Air-Monarch-IV-Pink",
+    price: 1485.95,
+    size: "4",
+    brand: "Nike",
+    description: "Elegancy is a style. do you have it? If not we got you coverd with our new range of Nike sneakers and a plater of other awesome selections.",
+    available: true,
+    quantity: 1
+  },
+
+];
+
+export function getSneakers() {
+  return sneakers;
+}
+export function getSneaker(id) {
+  return sneakers.find(
+    sneaker => sneaker.id === id
+  );
+ 
+}
